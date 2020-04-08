7a27054544e1abedca8d967c7cc9947739dc8a9a
Ash84 <louis.tromel@gmail.com>
2020-04-08 14:27:08 +0200

    Makes admin's password more secure

--- src/main/resources/--data.sql
+++ src/main/resources/--data.sql
@@ -1,10 +1,10 @@
-INSERT INTO user VALUES(0,'$2a$10$Hr3xJHIpL/EO7VO5jCpLOOfaA8I3EE9xYpLVcDHUcKXdEamZQzY4m','Administrateur', 2,'$2a$10$Hr3xJHIpL/EO7VO5jCpLOO');
+INSERT INTO user VALUES(0,'$2a$10$obEn8UaeTQViJGY2gM44fOXuxcV36ADuhN6cpQvRpnZ27XWymvzau','Administrateur', 2,'$2a$10$obEn8UaeTQViJGY2gM44fO');
 INSERT INTO user VALUES(111,'$2a$10$6efRgg5N/Bjp.Xv6vyF1Ie2OZIL.mWed2BuuhycjwEoWWdSD3C7tS','khalid', 2,'$2a$10$6efRgg5N/Bjp.Xv6vyF1Ie');
 
-INSERT INTO quizzes VALUES(15, 1, 'Example');
-INSERT INTO questions VALUES(100, 'BLABAL', 'Quel animal a un seul pied', 15, 1, 'bouton');
-INSERT INTO questions VALUES(101, 'BLAeBAL', "Qu'est-ce qui compose l'eau?", 15, 1, 'checkbox');
-INSERT INTO questions VALUES(103, 'BLABAL', 'Quel est le précédent président francais? (prénom nom)', 15, 1, 'texte');
+INSERT INTO quizzes VALUES(15, 1, 'Exemple');
+INSERT INTO questions VALUES(100, 'Des yeux exhorbités !', 'Quel animal a un seul pied', 15, 1, 'bouton');
+INSERT INTO questions VALUES(101, 'Vraiment, un indice pour ça?', "Qu'est-ce qui compose l'eau?", 15, 1, 'checkbox');
+INSERT INTO questions VALUES(103, 'Avec les majuscules !', 'Quel est le précédent président francais? (prénom nom)', 15, 1, 'texte');
 
 INSERT INTO answers VALUES(101, 'Un escargot', 100, 1, 1);
 INSERT INTO answers VALUES(102, 'Un unijambiste', 100, 0, 0);

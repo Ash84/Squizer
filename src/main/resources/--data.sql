e5ccdea77fd34522a8abca0f025c999da225f24a
Ash84 <louis.tromel@gmail.com>
2020-04-15 17:47:02 +0200

    Update --data.sql

--- src/main/resources/--data.sql
+++ src/main/resources/--data.sql
@@ -1,22 +1,174 @@
-INSERT INTO user VALUES(0,'$2a$10$obEn8UaeTQViJGY2gM44fOXuxcV36ADuhN6cpQvRpnZ27XWymvzau','Administrateur', 2,'$2a$10$obEn8UaeTQViJGY2gM44fO');
-INSERT INTO user VALUES(111,'$2a$10$6efRgg5N/Bjp.Xv6vyF1Ie2OZIL.mWed2BuuhycjwEoWWdSD3C7tS','khalid', 2,'$2a$10$6efRgg5N/Bjp.Xv6vyF1Ie');
+-- MySQL dump 10.13  Distrib 5.7.29, for Linux (x86_64)
+--
+-- Host: localhost    Database: Quizz
+-- ------------------------------------------------------
+-- Server version	5.7.29-0ubuntu0.18.04.1
 
-INSERT INTO quizzes VALUES(15, 1, 'Exemple');
-INSERT INTO questions VALUES(100, 'Des yeux exhorbités !', 'Quel animal a un seul pied', 15, 1, 'bouton');
-INSERT INTO questions VALUES(101, 'Vraiment, un indice pour ça?', "Qu'est-ce qui compose l'eau?", 15, 1, 'checkbox');
-INSERT INTO questions VALUES(103, 'Avec les majuscules !', 'Quel est le précédent président francais? (prénom nom)', 15, 1, 'texte');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-INSERT INTO answers VALUES(101, 'Un escargot', 100, 1, 1);
-INSERT INTO answers VALUES(102, 'Un unijambiste', 100, 0, 0);
-INSERT INTO answers VALUES(103, 'Le chat', 100, 0, 0);
-INSERT INTO answers VALUES(104, 'Emmanuel Macron', 100, 0, 0);
-INSERT INTO answers VALUES(105, 'Un éléphant', 100, 0, 0);
+--
+-- Table structure for table `answers`
+--
 
-INSERT INTO answers VALUES(111, "De l'hydrogène", 101, 1, 1);
-INSERT INTO answers VALUES(112, 'De la mémoire', 101, 1, 0);
-INSERT INTO answers VALUES(113, "De l'oxygène", 101, 1, 1);
-INSERT INTO answers VALUES(114, 'De la chlorure de sodium', 101, 1, 0);
-INSERT INTO answers VALUES(115, 'Du carbone', 101, 1, 0);
+DROP TABLE IF EXISTS `answers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `answers` (
+  `answerid` int(11) NOT NULL,
+  `answer` varchar(255) DEFAULT NULL,
+  `questionid` int(11) NOT NULL,
+  `value` double NOT NULL,
+  `verify` int(11) NOT NULL,
+  PRIMARY KEY (`answerid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO answers VALUES(116, 'François Hollande', 103, 1, 1);
-INSERT INTO results VALUES(100, 7, 15, 3, 1, 0);
+--
+-- Dumping data for table `answers`
+--
+
+LOCK TABLES `answers` WRITE;
+/*!40000 ALTER TABLE `answers` DISABLE KEYS */;
+INSERT INTO `answers` VALUES (1,'380 000 km2',1,0,0),(10,'440 000 km2',1,0,0),(2,'550 000 km2',1,1,1),(3,'660 000 km2',1,0,0),(4,'Mont Blanc',2,1,1),(23,'Rhin',3,1,1),(24,'Loire',3,0,0),(25,'Toulourenc',3,0,0),(27,'Le mont Ventoux',4,0.5,0),(28,'Le mont Blanc',4,1,1),(29,'Le mont Saint Michel',4,0.5,0);
+/*!40000 ALTER TABLE `answers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hibernate_sequence`
+--
+
+DROP TABLE IF EXISTS `hibernate_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+LOCK TABLES `hibernate_sequence` WRITE;
+/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
+INSERT INTO `hibernate_sequence` VALUES (32),(32),(32),(32),(32);
+/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `questions` (
+  `questionid` int(11) NOT NULL,
+  `hint` varchar(255) DEFAULT NULL,
+  `question` varchar(255) DEFAULT NULL,
+  `quiz` int(11) NOT NULL,
+  `state` int(11) NOT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`questionid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questions`
+--
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (1,'La France est le 41eme pays le plus vaste du monde','Combien de superficie environ pour l\'ensemble du pays?',1,1,'bouton'),(2,'C\'est le mont le plus haut du massif du même nom !','Quelle est la montagne la plus haute de France',1,1,'texte'),(3,'C\'est pas dans le nord.','Quel est le fleuve le plus long de France ?',1,1,'bouton'),(4,'Ils s\'en disputent carrément la propriété !','Quelle est La spécialité de la ville Laguiole?',1,1,'checkbox');
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `quizzes`
+--
+
+DROP TABLE IF EXISTS `quizzes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `quizzes` (
+  `quiz_id` int(11) NOT NULL,
+  `state` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`quiz_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `quizzes`
+--
+
+LOCK TABLES `quizzes` WRITE;
+/*!40000 ALTER TABLE `quizzes` DISABLE KEYS */;
+INSERT INTO `quizzes` VALUES (1,1,'Géographie française');
+/*!40000 ALTER TABLE `quizzes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `results`
+--
+
+DROP TABLE IF EXISTS `results`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `results` (
+  `resultid` int(11) NOT NULL,
+  `maxscore` double NOT NULL,
+  `quizid` int(11) NOT NULL,
+  `score` double NOT NULL,
+  `tries` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  PRIMARY KEY (`resultid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `userid` int(11) NOT NULL,
+  `hashed_password` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `role` int(11) NOT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (0,'$2a$10$NiTZ.nupWUmC8PDAblOSduLruISAin6l4CStphgqWll00F5Obvo3a','Administrateur',2,'$2a$10$NiTZ.nupWUmC8PDAblOSdu');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-15 16:21:12
